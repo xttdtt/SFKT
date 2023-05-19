ea02415884f0c15c46f76527cf1d5f3df491adb5
xttdtt <31698138+xttdtt@users.noreply.github.com>
2023-05-19 18:15:41 +0800

    Update HyperParameter.py

--- HyperParameter.py
+++ HyperParameter.py
@@ -1,9 +1,28 @@
+import os
+import tensorflow as tf
+
+
+# calculate the total number of parameters
+def calculate_parameter(savefile):
+    modelfolder = os.path.join(dataset, 'Model')
+    tf.train.import_meta_graph(modelfolder + '/' + savefile + '.ckpt.meta')
+    variables = tf.trainable_variables()
+    total_parameters = 0
+    for variable in variables:
+        shape = variable.get_shape()
+        variable_parameters = 1
+        for dim in shape:
+            variable_parameters *= dim.value
+        total_parameters += variable_parameters
+    return total_parameters
+
+
 """Here are some fixed parameters"""
-# maximum number of program training
+# the maximum number of program training
 epochs = 1000
-# batch sample size
+# batch size of samples
 bs = 256
-# early stop mechanism
+# early-stop mechanism
 early_stop = 20
 # dropout ratio
 keep_rate = 0.5
@@ -14,7 +33,7 @@ split_rate = 0.7
 Here are some non-fixed parameters
 When performing ablation experiments, you can modify the values here
 """
-# dataset you need to run, this program only supports two datasets:Assist09 and Assist12
+# this program only supports two datasets:Assist09 and Assist12
 dataset = "Assist09"
 # learning rate
 lr = 0.01
